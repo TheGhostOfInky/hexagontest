2fd3b7fab77e8776bff90576006a15df3756dc6b
Quark <72112478+QUARK88@users.noreply.github.com>
2021-07-15 18:07:15 -0400

    Create ideologies.js

---
+++ ideologies.js
@@ -0,0 +1,13 @@
+ideologies = [
+    {
+        "name": "Liberalism",
+        "stats": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    }
+]
