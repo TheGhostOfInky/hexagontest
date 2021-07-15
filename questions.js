00ebbeae34af4e18a045b79a788ea342165741ac
Quark <72112478+QUARK88@users.noreply.github.com>
2021-07-15 17:48:53 -0400

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,200 @@
+questions = [
+    {
+        "question": "The state should enforce a class system.",
+        "effect": {
+            "b1": 1,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Elites should have less power over the lives of everyday people.",
+        "effect": {
+            "b1": -1,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Property should be the basis for our society.",
+        "effect": {
+            "b1": 0,
+            "b2": 1,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Private property should be abolished.",
+        "effect": {
+            "b1": 0,
+            "b2": -1,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "A market economy is desirable.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Markets require regulation.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": -1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Nobody should hold power over another individual.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 1,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Hierarchy is sometimes necessary.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": -1,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Direct democracy is good.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 1,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Welfare makes people lazy.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": -1,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "All industry should be nationalised.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 1,
+        }
+    },
+    {
+        "question": "The state should be less involved in the economy.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": -1,
+        }
+    },
+    {
+        "question": "Hierarchy will arise in any society.",
+        "effect": {
+            "b1": 1,
+            "b2": 1,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Chasing equality will lead to authoritarianism.",
+        "effect": {
+            "b1": 0,
+            "b2": 1,
+            "b3": 1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "The state should be abolished.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 1,
+            "b4": 1,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Workers should own the means of production.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 1,
+            "b5": 1,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Money should be abolished.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 1,
+            "b6": 1,
+        }
+    },
+    {
+        "question": "The economy must be top down to ensure efficiency.",
+        "effect": {
+            "b1": 1,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 1,
+        }
+    }
+]
